53817a42b7d30f5914b530c8020a0780be503f81
CoYe <wiseman.kostya@gmail.com>
2013-06-16 23:03:20 +0300

    Banners

--- banners/src/org/study/banners/Main.java
+++ banners/src/org/study/banners/Main.java
@@ -6,6 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
          BannersStorage banners = new BannersStorage();
-         banners.populateBannersArray("Car");
+         banners.getBannersArray("Car");
     }
 }
