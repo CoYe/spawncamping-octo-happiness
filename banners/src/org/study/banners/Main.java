3d5f3bd9f394d82403601a2571c4a6970526992e
CoYe <wiseman.kostya@gmail.com>
2013-06-18 18:18:58 +0300

    update

--- banners/src/org/study/banners/Main.java
+++ banners/src/org/study/banners/Main.java
@@ -8,16 +8,36 @@ public class Main {
 
     public static void main(String[] args) {
         String topicQuery;
-        String sortingType = "";
+        int sortingType;
         System.out.println("Enter Required Topic:");
         Arrays .deepToString(BannersStorage .bannersTopicsArray );
         Scanner input = new Scanner(System .in);
         topicQuery = input .nextLine();
-        System.out.println("Select printout type: by default - normal, m - mixed, s - sorted");
-        sortingType = input.nextLine();
+        System.out.println("Select printout type: by default - normal, 1 - mixed, 2 - sorted");
+        sortingType = input.nextInt();
         switch(sortingType){
-
+            case (1):
+                getMixedBannersArray(topicQuery);
+                break;
+            case (2):
+                getSortedBannersArray(topicQuery);
+                break;
+            default:
+                getNormalBannersArray(topicQuery);
+                break;
         }
+
+    }
+
+    private static void getMixedBannersArray(String topic){
+
+    }
+
+    private static void getSortedBannersArray(String topic){
+
+    }
+
+    private static void getNormalBannersArray(String topic){
 
     }
 }
