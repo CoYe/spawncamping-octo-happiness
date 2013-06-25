c0fe5da85b7f0b2cb75117bf6a6ba2b4f1e72642
CoYe <wiseman.kostya@gmail.com>
2013-06-25 17:06:30 +0300

    new file:   banners/src/org/study/banners/DataInput.java 	new file:   banners/src/org/study/banners/DataOutput.java 	new file:   banners/src/org/study/banners/MIxedBanners.java 	new file:   banners/src/org/study/banners/SortedBanners.java 	modified:   banners/src/org/study/banners/BannersArray.java 	modified:   banners/src/org/study/banners/BannersStorage.java 	modified:   banners/src/org/study/banners/DataInput.java 	modified:   banners/src/org/study/banners/DataOutput.java 	modified:   banners/src/org/study/banners/MIxedBanners.java 	modified:   banners/src/org/study/banners/Main.java 	modified:   banners/src/org/study/banners/SortedBanners.java

---
+++ banners/src/org/study/banners/DataInput.java
@@ -0,0 +1,11 @@
+package org.study.banners;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: CoYe
+ * Date: 21.06.13
+ * Time: 13:19
+ * To change this template use File | Settings | File Templates.
+ */
+public class DataInput {
+}
