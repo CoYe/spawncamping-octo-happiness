3101dd1f6c0033afae6ddf8c033caaed4c23284d
CoYe <wiseman.kostya@gmail.com>
2013-06-25 20:31:51 +0300

    new file:   banners/src/org/study/banners/UnsortedBanners.java

---
+++ banners/src/org/study/banners/UnsortedBanners.java
@@ -0,0 +1,16 @@
+package org.study.banners;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: CoYe
+ * Date: 25.06.13
+ * Time: 20:24
+ * To change this template use File | Settings | File Templates.
+ */
+public class UnsortedBanners extends AbstractBannersArrayGetter {
+    public String[] getBannersArray(String[] bannersArray){
+        String[] resultArray;
+        resultArray = bannersArray;
+        return resultArray;
+    }
+}
